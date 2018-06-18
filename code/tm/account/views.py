5054cf749ef52f45dc97fa350cec2c10821224d1
Likit Preeyanon <likit.pre@mahidol.edu>
2018-06-18 15:49:31 +0700

    added django project with wagtail as a cms
    added Facebook and Google authentication page
    
    added a wagtail home page

---
+++ code/tm/account/views.py
@@ -0,0 +1,94 @@
+import requests
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, UserRegistrationForm
+from py2neo import Graph
+
+graph = Graph(host='neo4j_db', password='_genius01_', scheme='bolt')
+
+# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+            else:
+                return HttpResponse('Disabled account')
+        else:
+            return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    social_user = request.user.social_auth.filter(
+        provider='facebook',
+    ).first()
+    if social_user: # try facebook login
+        url = 'https://graph.facebook.com/{0}/'.format(social_user.uid)
+        res = requests.get(url, {'fields':'email,picture,name,id,location', 'access_token':social_user.extra_data['access_token']}).json()
+        query = 'match (p:Person)-[:STUDIED]-(info:StudyInfo) where p.firstname_en="%s" and p.lastname_en="%s" return info'
+        query = query % (request.user.first_name.lower(), request.user.last_name.lower())
+        studyinfo = list(graph.run(query))[0].get('info')
+        country = studyinfo.get('country','')
+        specialty = studyinfo.get('specialty','')
+        fos = studyinfo.get('field_of_study','')
+        degree = studyinfo.get('degree_title', '')
+        return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard',
+                   'picture_url': res['picture']['data']['url'],
+                   'name': res['name'],
+                   'email': res.get('email', None),
+                   'country': country,
+                   'specialty': specialty,
+                   'fos': fos,
+                   'degree': degree,
+                   })
+    else: # try google login
+        social_user = request.user.social_auth.filter(
+            provider='google-oauth2',
+        ).first()
+        google_token = social_user.extra_data['access_token']
+        url = 'https://www.googleapis.com/oauth2/v1/userinfo'.format(social_user.uid)
+        res = requests.get(url, {'access_token': google_token, 'alt': 'json'}).json()
+        return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard',
+                   'picture_url': res.get('picture'),
+                   'name': res.get('name'),
+                   'email': res.get('email', None),
+                   })
+    return render(request,
+                'account/dashboard.html',
+                {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
---
+++ code/tm/tm/urls.py
@@ -0,0 +1,33 @@
+"""tm URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.conf import settings
+from django.urls import path
+from django.contrib import admin
+from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.core import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('cmsadmin/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    path('social-auth/',
+         include('social_django.urls', namespace='social')),
+    path('', include(wagtail_urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
