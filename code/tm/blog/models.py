28fc2c9215e9c724dd7555441c09a6ad29c814d2
Likit Preeyanon <likit.pre@mahidol.edu>
2018-06-20 19:44:50 +0700

    added tags and routes that allow querying posts by date

--- code/tm/blog/models.py
+++ code/tm/blog/models.py
@@ -1,6 +1,9 @@
 import datetime
 from datetime import date
+from django import forms
+from django.http import Http404, HttpResponse
 from django.db import models
+
 from django.utils.dateformat import DateFormat
 from django.utils.formats import date_format
 from wagtail.core.models import Page
@@ -11,7 +14,6 @@ from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from taggit.models import TaggedItemBase, Tag as TaggitTag
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.tags import ClusterTaggableManager
-from django import forms
 # Create your models here.
 
 class BlogPageTag(TaggedItemBase):
@@ -49,10 +51,35 @@ class BlogPage(RoutablePageMixin, Page):
         context = super(BlogPage, self).get_context(request, *args, **kwargs)
         context['posts'] = self.posts
         context['blog_page'] = self
+        context['search_type'] = getattr(self, 'search_type', "")
+        context['search_term'] = getattr(self, 'search_term', "")
         return context
 
     def get_posts(self):
         return PostPage.objects.descendant_of(self).live()
+
+    @route(r'^(\d{4})/$')
+    @route(r'^(\d{4})/(\d{2})/$')
+    @route(r'^(\d{4})/(\d{2})/(\d{2})/$')
+    def post_by_date(self, request, year, month=None, day=None, *args, **kwargs):
+        self.posts = self.get_posts().filter(date__year=year)
+        self.search_type = 'date'
+        self.search_term = year
+        if month:
+            self.posts = self.posts.filter(date__month=month)
+            df = DateFormat(date(int(year), int(month), 1))
+            self.search_term = df.format('F Y')
+        if day:
+            self.posts = self.posts.filter(date__day=day)
+            self.search_term = date_format(date(int(year), int(month), int(day)))
+        return Page.serve(self, request, *args, **kwargs)
+
+    @route(r'^(\d{4})/(\d{2})/(\d{2})/(.+)/$')
+    def post_by_date_slug(self, request, year, month, day, slug, *args, **kwargs):
+        post_page = self.get_posts().filter(slug=slug).first()
+        if not post_page:
+            raise Http404
+        return Page.serve(post_page, request, *args, **kwargs)
 
     @route(r'^tag/(?P<tag>[-\w]+)/$')
     def post_by_tag(self, request, tag, *args, **kwargs):
