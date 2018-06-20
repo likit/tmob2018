0081954e2506128f3ebfa4a495e103a00af60df5
Likit Preeyanon <likit.pre@mahidol.edu>
2018-06-20 16:49:04 +0700

    added PageRoutableMixin to blog page
    updated the blog model
    
    changed the layout of the blog and post page

--- code/tm/blog/models.py
+++ code/tm/blog/models.py
@@ -1,8 +1,13 @@
+import datetime
+from datetime import date
 from django.db import models
+from django.utils.dateformat import DateFormat
+from django.utils.formats import date_format
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, PageChooserPanel
 from wagtail.snippets.models import register_snippet
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from taggit.models import TaggedItemBase, Tag as TaggitTag
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.tags import ClusterTaggableManager
@@ -19,16 +24,45 @@ class Tag(TaggitTag):
         proxy = True
 
 
-class BlogPage(Page):
+class BlogPage(RoutablePageMixin, Page):
     description = models.CharField(max_length=255, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('description')
     ]
 
+    def get_context(self, request, *args, **kwargs):
+        context = super(BlogPage, self).get_context(request, *args, **kwargs)
+        context['posts'] = self.posts
+        context['blog_page'] = self
+        return context
+
+    def get_posts(self):
+        return PostPage.objects.descendant_of(self).live()
+
+    @route(r'^tag/(?P<tag>[-\w]+)/$')
+    def post_by_tag(self, request, tag, *args, **kwargs):
+        self.search_type = 'tag'
+        self.search_term = tag
+        self.posts = self.get_posts().filter(tags__slug=tag)
+        return Page.serve(self, request, *args, **kwargs)
+
+    @route(r'^categoty/(?P<category>[-\w]+)/$')
+    def post_by_category(self, request, category, *args, **kwargs):
+        self.search_type = 'category'
+        self.search_term = category
+        self.posts = self.get_posts().filter(categories__slug=category)
+        return Page.serve(self, request, *args, **kwargs)
+
+    @route(r'^$')
+    def post_list(self, request, *args, **kwargs):
+        self.posts = self.get_posts()
+        return Page.serve(self, request, *args, **kwargs)
+
 
 class PostPage(Page):
     body = RichTextField(blank=True)
+    date = models.DateTimeField(verbose_name="Post date", default=datetime.datetime.today)
     categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
     tags = ClusterTaggableManager(through='blog.BlogPageTag', blank=True)
     content_panels = Page.content_panels + [
@@ -36,6 +70,19 @@ class PostPage(Page):
         FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
         FieldPanel('tags'),
     ]
+
+    settings_panels = Page.settings_panels + [
+        FieldPanel('date'),
+    ]
+
+    @property
+    def blog_page(self):
+        return self.get_parent().specific
+
+    def get_context(self, request, *args, **kwargs):
+        context = super(PostPage, self).get_context(request, *args, **kwargs)
+        context['blog_page'] = self.blog_page
+        return context
 
 
 @register_snippet
--- code/tm/tm/settings.py
+++ code/tm/tm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
+    'wagtail.contrib.routable_page',
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
