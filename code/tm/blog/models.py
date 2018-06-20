27d2b6f2975f52804c342aeaaa153595aeef552c
Likit Preeyanon <likit.pre@mahidol.edu>
2018-06-20 10:30:21 +0700

    added new theme
    added categories to posts
    
    updated post model
    
    updated settings file by removing staticfiles_dir
    
    added some templates

--- code/tm/blog/models.py
+++ code/tm/blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.snippets.models import register_snippet
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from django import forms
 # Create your models here.
 
 class BlogPage(Page):
@@ -10,3 +13,30 @@ class BlogPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('description')
     ]
+
+
+class PostPage(Page):
+    body = RichTextField(blank=True)
+    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname='full'),
+        FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+    ]
+
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=80)
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('slug'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
