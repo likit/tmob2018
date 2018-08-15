a765f49eb25c19decc6c7d4f692da88bba297f38
Likit Preeyanon <likit.pre@mahidol.edu>
2018-08-15 15:50:07 +0700

    added views and urls for blog language switch

--- code/tm/blog/views.py
+++ code/tm/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+def switch_lang(request):
+    if request.session.get('language', None) is None:
+        request.session['language'] = 'th'
+    elif request.session.get('language') == 'en':
+        request.session['language'] = 'th'
+    elif request.session.get('language') == 'th':
+        request.session['language'] = 'en'
+    print(request.session.get('language'))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
