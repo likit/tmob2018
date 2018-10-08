922d2364b028291aa27526a9ce9050ec2fdcb724
Likit Preeyanon <likit.pre@mahidol.edu>
2018-10-08 11:07:26 +0700

    added some keyword-related models
    added relationships for the field pub and other related models
    
    added migration revision files

--- code/tm/utils/pubdw.py
+++ code/tm/utils/pubdw.py
@@ -6,6 +6,51 @@ from sqlalchemy.orm import Session, relationship, backref
 Base = declarative_base()
 
 
+class Keyword(Base):
+    __tablename__ = 'keywords'
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    word_en = Column(String(64), nullable=False, index=True)
+    word_th = Column(String(64), nullable=False, index=True)
+    abstract_id = Column(Integer(), ForeignKey('abstracts.id'))
+    count = Column(Integer())
+    word_lists = relationship('KeywordList', backref='keyword')
+    noun_chunk_word_lists = relationship('NounChunkWordList', backref='keyword')
+
+    def __str__(self):
+        return self.word_en
+
+
+class NounChunk(Base):
+    __tablename__ = 'noun_chunks'
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    chunk_en = Column(String(255), nullable=False)
+    chunk_th = Column(String(255), nullable=False)
+    is_auth_keywords = Column(Boolean(), default=False)
+    keywords = relationship('NounChunkWordList', backref='noun_chunk')
+    nounchunk_lists = relationship('NounChunkList', backref='noun_chunk')
+
+
+class KeywordList(Base):
+    __tablename__ = 'keyword_lists'
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    keyword_id = Column(Integer, ForeignKey('keywords.id'))
+    field_pub_id = Column(Integer, ForeignKey('field_pubs.id'))
+
+
+class NounChunkWordList(Base):
+    __tablename__ = 'noun_chunk_word_lists'
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    keyword_id = Column(Integer, ForeignKey('keywords.id'))
+    noun_chunk_id = Column(Integer, ForeignKey('noun_chunks.id'))
+
+
+class NounChunkList(Base):
+    __tablename__ = 'noun_chunk_lists'
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    noun_chunk_id = Column(Integer, ForeignKey('noun_chunks.id'))
+    field_pub_id = Column(Integer, ForeignKey('field_pubs.id'))
+
+
 class Field(Base):
     __tablename__ = 'fields'
     id = Column(Integer(), primary_key=True, autoincrement=True)
@@ -20,7 +65,9 @@ class FieldPub(Base):
     id = Column(Integer(), primary_key=True, autoincrement=True)
     field_id  = Column(Integer, ForeignKey('fields.id'))
     abstract_id = Column(Integer, ForeignKey('abstracts.id'))
-    author_list_id = Column(Integer, ForeignKey('author_lists.id'))
+    authors = relationship('AuthorList', backref='field_pub')
+    keywords = relationship('KeywordList', backref='field_pub')
+    nounchunks = relationship('NounChunkList', backref='field_pub')
 
 
 class Abstract(Base):
@@ -35,6 +82,17 @@ class Abstract(Base):
     cited = Column(Integer())
     pub_date = Column(Date())
     fields = relationship('FieldPub', backref='abstract')
+    words = relationship('Keyword', backref='abstract')
+
+
+class Affiliation(Base):
+    __tablename__ = 'affils'
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    scopus_id = Column(String(64))
+    name = Column(String())
+    country = Column(String())
+    city = Column(String())
+    pub_lists = relationship('AuthorList', backref='affil')
 
 
 class Author(Base):
@@ -45,8 +103,13 @@ class Author(Base):
     scopus_id = Column(String(64))
     pub_lists = relationship('AuthorList', backref='author')
 
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
 
 class AuthorList(Base):
     __tablename__ = 'author_lists'
     id = Column(Integer(), primary_key=True, autoincrement=True)
     author_id = Column(Integer, ForeignKey('authors.id'))
+    affil_id = Column(Integer, ForeignKey('affils.id'))
+    field_pub_id = Column(Integer, ForeignKey('field_pubs.id'))
