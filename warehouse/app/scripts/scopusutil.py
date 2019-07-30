e8e161762cebbbf3c058eff9b9f74e4c978cf0a2
Likit Preeyanon <likit.pre@mahidol.edu>
2019-07-30 11:03:12 +0700

    Added a command group and load-by-keyword command. Added some options. Turned load-by-year function to a command.

--- warehouse/app/scripts/scopusutil.py
+++ warehouse/app/scripts/scopusutil.py
@@ -7,10 +7,13 @@ import sys
 import requests
 import json
 import time
+import click
 from itertools import islice
 from datetime import datetime
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.sql import insert, update, select
+
+CURRENT_YEAR = datetime.today().year
 
 meta = MetaData()
 pub_engine = create_engine('postgresql+psycopg2://postgres@postgres_db/stagedb')
@@ -18,13 +21,92 @@ pub_engine = create_engine('postgresql+psycopg2://postgres@postgres_db/stagedb')
 pub_con = pub_engine.connect()
 pub_table = Table('pubs', meta, autoload=True, autoload_with=pub_engine)
 
-SCOPUS_API_KEY = os.environ['SCOPUS_API_KEY']
 
-ITEM_PER_PAGE = 25
-SLEEPTIME = 5
+@click.group()
+def load():
+    pass
 
 
-def load(year, doctype='ar'):
+@click.command()
+@click.option('-k', '--keyword',
+                help='search keyword or term',
+                required=True)
+@click.option('-y', '--year',
+                help='published year',
+                default=CURRENT_YEAR, show_default=True)
+@click.option('--doctype', type=click.Choice(
+                            ['ar', 'ab', 'ip', 'bk', 'bz',
+                            'ch', 'cp', 'cr', 'ed', 'er',
+                            'le', 'no', 'pr', 're', 'sh'
+                            ]),
+                            help='article types', default='ar')
+@click.option('--scopus-api-key', envvar='SCOPUS_API_KEY')
+@click.option('--item-per-page', default=25, type=int)
+@click.option('--sleep-time', default=10, type=int)
+def by_keyword(keyword, year, doctype='ar'):
+    query = 'TITLE-ABS-KEY({}) PUBYEAR = {} AFFILCOUNTRY(thailand) DOCTYPE({})'.format(keyword, year, doctype)
+    params = {'apiKey': SCOPUS_API_KEY, 'query': query, 'httpAccept': 'application/json', 'view': 'COMPLETE'}
+    url = 'http://api.elsevier.com/content/search/scopus'
+    r = requests.get(url, params).json()
+
+    print('Total articles = {}'.format(r.get('search-results').get('opensearch:totalResults')))
+
+    stats['keyword'] = keyword
+    stats['year'] = year
+    stats['articles'] = r.get('search-results').get('entry')
+    stats['authors'] = []
+
+    for article in r.get('search-results').get('entry'):
+        identifier = article.get('dc:identifier', None)
+        if identifier:
+            scopus_id = identifier.split(':')[1]
+        else:
+            continue
+
+        params = {'apiKey': SCOPUS_API_KEY, 'query': query, 'httpAccept': 'application/json',
+                  'view': 'FULL'}
+        url = 'http://api.elsevier.com/content/abstract/scopus_id/' + scopus_id
+        print('Downloading from {}'.format(url))
+        result = requests.get(url, params=params).json()
+        abstract_resp = result.get('abstracts-retrieval-response')
+        if abstract_resp:
+            citation_count = abstract_resp.get('coredata').get('citedby-count', 0)
+            pub_date = abstract_resp.get('coredata').get('prism:coverDate')
+            pub_date = datetime.strptime(pub_date, '%Y-%m-%d')
+
+            s = select([pub_table]).where(pub_table.c.scopus_id == scopus_id)
+            _pub = pub_con.execute(s).first()
+            if _pub:
+                print('\tAlready loaded. Updating citation count..'.format(scopus_id))
+                u = update(pub_table).where(pub_table.c.scopus_id == scopus_id)
+                u = u.values(cited_count=int(citation_count))
+                _res = pub_con.execute(u)
+                continue  # pub already loaded, move on!
+            else:
+                ins = insert(pub_table).values(
+                    scopus_id=scopus_id,
+                    cited_count=int(citation_count),
+                    pub_date=pub_date,
+                    data=result,
+                    doctype=doctype
+                )
+                pub_con.execute(ins)
+
+
+@click.command()
+@click.option('-y', '--year',
+                help='published year',
+                default=CURRENT_YEAR, show_default=True)
+@click.option('--doctype', type=click.Choice(
+                            ['ar', 'ab', 'ip', 'bk', 'bz',
+                            'ch', 'cp', 'cr', 'ed', 'er',
+                            'le', 'no', 'pr', 're', 'sh'
+                            ]),
+                            help='article types', default='ar')
+@click.option('--scopus-api-key', envvar='SCOPUS_API_KEY')
+@click.option('--item-per-page', default=25, type=int)
+@click.option('--sleep-time', default=10, type=int)
+def by_year(year, doctype='ar'):
     query = 'PUBYEAR = {} AFFILCOUNTRY(thailand) DOCTYPE({})'.format(year, doctype)
     params = {'apiKey': SCOPUS_API_KEY, 'query': query, 'httpAccept': 'application/json', 'view': 'COMPLETE'}
     search_url = 'http://api.elsevier.com/content/search/scopus'
@@ -33,11 +115,11 @@ def load(year, doctype='ar'):
     total_articles = r.get('search-results', {}).get('opensearch:totalResults', 0)
     print('Total articles = {}'.format(total_articles))
 
-    for start in range(75, 200, ITEM_PER_PAGE):
+    for start in range(75, 200, item_per_page):
         search_params = {'apiKey': SCOPUS_API_KEY,
                   'query': query,
                   'start': start,
-                  'count': ITEM_PER_PAGE,
+                  'count': item_per_page,
                   'httpAccept': 'application/json',
                   'view': 'COMPLETE'}
 
@@ -88,9 +170,12 @@ def load(year, doctype='ar'):
                     )
                     pub_con.execute(ins)
         print('Sleeping...')
-        time.sleep(10)
+        time.sleep(sleep_time)
+
+
+load.add_command(by_keyword)
+load.add_command(by_year)
 
 
 if __name__ == '__main__':
-    year = sys.argv[1]
-    load(year, doctype='ar')
+    load()
